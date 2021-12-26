0520e4977ca933981256082c541b62e509ee6462
Finn Then <71402477+finebythen@users.noreply.github.com>
2021-12-26 10:35:51 +0100

    removed secret key

--- project_ajax/settings.py
+++ project_ajax/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w*3#r)tb-1zq+(luu9d@ieo7rfw)$kqn#h)itg7v2m%598tn*('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
